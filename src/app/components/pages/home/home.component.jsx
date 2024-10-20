47e65748aa424190d9e19f47dfb93ef3d2179878
Alan Rocha <alanrochaarg2001@gmail.com>
2024-10-20 13:07:53 -0300

    feat: add new layout

--- src/app/components/pages/home/home.component.jsx
+++ src/app/components/pages/home/home.component.jsx
@@ -1,144 +1,117 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFade, Autoplay, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/effect-fade";
-import "swiper/css/pagination";
-import "../home/home.component.css";
+import React, { useState, useEffect } from "react";
+import { Carousel } from "primereact/carousel";
+import { Button } from "primereact/button";
+import { Tag } from "primereact/tag";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "./home.component.css";
 
 const HomeComponent = () => {
+  const [movies, setMovies] = useState([]);
+
+  const responsiveOptions = [
+    {
+      breakpoint: "1400px",
+      numVisible: 3,
+      numScroll: 1,
+    },
+    {
+      breakpoint: "1199px",
+      numVisible: 2,
+      numScroll: 1,
+    },
+    {
+      breakpoint: "767px",
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
+
+  useEffect(() => {
+    const movieData = [
+      {
+        title: "Alien: Romulus",
+        image:
+          "https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg",
+        // status: "NOVO",
+      },
+      {
+        title: "A Substância",
+        image:
+          "https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg",
+        // status: "NOVO",
+      },
+      {
+        title: "Batman: Arkham Knight",
+        image:
+          "https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg",
+        // status: "NOVO",
+      },
+      {
+        title: "Batman: Arkham Knight",
+        image:
+          "https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg",
+        // status: "NOVO",
+      },
+      {
+        title: "Batman: Arkham Knight",
+        image:
+          "https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg",
+        // status: "NOVO",
+      },
+      {
+        title: "Batman: Arkham Knight",
+        image:
+          "https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg",
+        status: "NOVO",
+      },
+      {
+        title: "Batman: Arkham Knight",
+        image:
+          "https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg",
+        status: "NOVO",
+      },
+    ];
+    setMovies(movieData);
+  }, []);
+
+  const movieTemplate = (movie) => {
+    return (
+      <div className="movie-card border-1 surface-border border-round m-2 text-center py-5 px-3">
+        <div className="mb-3">
+          <img
+            src={movie.image}
+            alt={movie.title}
+            className="w-6 shadow-2 movie-image"
+          />
+        </div>
+        <div>
+          <h4 className="mb-1">{movie.title}</h4>
+          <Tag value={movie.status} className="mt-2"></Tag>
+          <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
+            <Button icon="pi pi-search" className="p-button p-button-rounded" />
+            <Button
+              icon="pi pi-star-fill"
+              className="p-button-success p-button-rounded"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="home-container">
-      <Swiper
-        spaceBetween={30}
-        effect="fade"
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        modules={[EffectFade, Autoplay, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img
-            src="https://cdn1.epicgames.com/400347196e674de89c23cc2a7f2121db/offer/AC%20KINGDOM%20PREORDER_STANDARD%20EDITION_EPIC_Key_Art_Wide_3840x2160-3840x2160-485fe17203671386c71bde8110886c7d.jpg"
-            alt="Assassin's Creed Valhalla"
-            className="carousel-image"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/4RKia895j9bcLcVEsLL1PB/dbd31dfe5049a8b65abbe13727d113ef/ac-rogue-heroBanner.jpg"
-            alt="Assassin's Creed Rogue"
-            className="carousel-image"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-            alt="Batman: Arkham Knight"
-            className="carousel-image"
-          />
-        </SwiperSlide>
-      </Swiper>
-
-      <section id="no-cinema" className="movies-section">
-        <h2 className="section-title">Playlists</h2>
-        <div className="movie-grid">
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="Alien: Romulus"
-              className="movie-image"
-            />
-            <h3 className="movie-title">Alien: Romulus</h3>
-          </div>
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="A Substância"
-              className="movie-image"
-            />
-            <h3 className="movie-title">A Substância</h3>
-          </div>
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="A Substância"
-              className="movie-image"
-            />
-            <h3 className="movie-title">A Substância</h3>
-          </div>
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="A Substância"
-              className="movie-image"
-            />
-            <h3 className="movie-title">A Substância</h3>
-          </div>
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="A Substância"
-              className="movie-image"
-            />
-            <h3 className="movie-title">A Substância</h3>
-          </div>
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="A Substância"
-              className="movie-image"
-            />
-            <h3 className="movie-title">A Substância</h3>
-          </div>
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="A Substância"
-              className="movie-image"
-            />
-            <h3 className="movie-title">A Substância</h3>
-          </div>
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="A Substância"
-              className="movie-image"
-            />
-            <h3 className="movie-title">A Substância</h3>
-          </div>
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="A Substância"
-              className="movie-image"
-            />
-            <h3 className="movie-title">A Substância</h3>
-          </div>
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="A Substância"
-              className="movie-image"
-            />
-            <h3 className="movie-title">A Substância</h3>
-          </div>
-        </div>
-      </section>
-
-      <section id="remakes" className="remakes-section">
-        <h2 className="section-title">Remakes de histórias marcantes!</h2>
-        <div className="movie-grid">
-          <div className="movie-card">
-            <img
-              src="https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbb9f69702d0a9c7da700/sharing_images/nok69j6m12kgovgokrbh.jpg"
-              alt="Título do Filme"
-              className="movie-image"
-            />
-            <h3 className="movie-title">Título do Filme</h3>
-          </div>
-        </div>
-      </section>
+      <h2 className="section-title">Filmes Populares</h2>
+      <Carousel
+        value={movies}
+        numScroll={1}
+        numVisible={3}
+        responsiveOptions={responsiveOptions}
+        itemTemplate={movieTemplate}
+        className="custom-carousel"
+      />
     </div>
   );
 };
