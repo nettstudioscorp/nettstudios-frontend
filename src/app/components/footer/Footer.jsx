0b52097cf7fd31d9c0d87cc75d32a17b0021c231
Alan Rocha <alanrochaarg2001@gmail.com>
2025-02-14 11:09:09 -0300

    chore: adding new `p` tag and changing/adding `favicon`

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const FooterComponent = () => {
           <img src={logo} alt="Logo" className="footer-logo" />
 
           <p className="footer-description">
-            NettStudios, focada no universo dos Games, Ti e do entretenimento
+            NettStudios, focada no universo dos Games e do entretenimento
             digital.{' '}
             {/*Desde 2015, o NettStudios
             produz e compartilha conteúdos de qualidade, incluindo gameplays,
@@ -59,13 +59,17 @@ const FooterComponent = () => {
       <br />
       <div className="footer-bottom">
         <p>
-          © Copyright 2015-2025 NettStudios - Games, TI, Entretenimento e
-          Criação de Conteúdo.
+          © Copyright 2015-2025 NettStudios - Games, Entretenimento e Criação
+          de Conteúdo.
         </p>
         <a href="/termos-de-uso">Termos de uso</a> &nbsp;
         <span style={{ color: 'white' }}>e</span> &nbsp;
         <a href="/politica-de-privacidade">Política de privacidade</a>
-        <p>Site: v0.0.9</p>
+        <br />
+        <p>
+          Desenvolvido por{' '}
+          <a href="https://alanrocha.vercel.app">Alan Rocha </a> | Site: v0.0.9
+        </p>
       </div>
     </footer>
   );
