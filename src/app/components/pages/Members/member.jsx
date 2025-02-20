eca3124facadab54e7f46a65d8be5ed4b2286954
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2025-02-20 17:54:21 -0300

    feat(member): Implement Members Page with GitHub links, uncomment member in `navbar`, add responsive styling, and remove authentication check on Members Page (#142)

--- src/app/components/pages/Members/member.jsx
+++ src/app/components/pages/Members/member.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { getMembers } from './member.service';
 import '../Members/member.css';
-import { useNavigate } from 'react-router-dom';
 
 const MemberList = () => {
-  const navigate = useNavigate();
-  const isAuthenticated = localStorage.getItem('isAuthenticated');
-
-  useEffect(() => {
-    if (isAuthenticated !== 'true') {
-      alert('Você precisa estar logado para acessar esta página.');
-      navigate('/login');
-    }
-  }, [isAuthenticated, navigate]);
-
   const members = getMembers();
 
   return (
-    <div className="member-list">
-      {members.map((member, index) => (
-        <div className="member-card" key={index}>
-          <img src={member.image} alt={member.name} className="member-image" />
-          <div className="member-role">{member.role}</div>
-          <div className="member-name">{member.name}</div>
-        </div>
-      ))}
+    <div>
+      <h1>Nossa Equipe</h1>
+      <div className="member-list">
+        {members.map((member, index) => (
+          <a
+            href={member.github}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={index}
+          >
+            <div className="member-card">
+              <img
+                src={member.image}
+                alt={member.name}
+                className="member-image"
+              />
+              <div className="member-role">{member.role}</div>
+              <div className="member-name">{member.name}</div>
+            </div>
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
